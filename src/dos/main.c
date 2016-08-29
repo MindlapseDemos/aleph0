63b624908206e1cbe56c2ce51cb170d3f6d91126
John Tsiombikas <nuclear@mutantstargoat.com>
2016-08-29 05:44:40 +0300

    missing includes

--- src/dos/main.c
+++ src/dos/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 #include "demo.h"
 #include "keyb.h"
+#include "mouse.h"
 #include "timer.h"
 #include "gfx.h"
 
 static int quit;
+static int use_mouse;
 
 int main(int argc, char **argv)
 {
@@ -14,6 +18,11 @@ int main(int argc, char **argv)
 
 	init_timer(100);
 	kb_init(32);
+
+	if((use_mouse = have_mouse())) {
+		set_mouse_limits(0, 0, fb_width, fb_height);
+		set_mouse(fb_width / 2, fb_height / 2);
+	}
 
 	if(!(fb_pixels = malloc(fbsize))) {
 		fprintf(stderr, "failed to allocate backbuffer\n");
@@ -37,7 +46,9 @@ int main(int argc, char **argv)
 		}
 		if(quit) goto break_evloop;
 
-		mouse_bmask = read_mouse(&mouse_x, &mouse_y);
+		if(use_mouse) {
+			mouse_bmask = read_mouse(&mouse_x, &mouse_y);
+		}
 
 		time_msec = get_msec();
 		demo_draw();

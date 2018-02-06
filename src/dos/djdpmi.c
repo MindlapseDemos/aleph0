c9a3eeccc46a19a4ab5e8b13839dfec9d05ea1d6
John Tsiombikas <nuclear@member.fsf.org>
2018-02-07 00:56:32 +0200

    fixed mistake in the DJGPP version of dpmi_alloc

--- src/dos/djdpmi.c
+++ src/dos/djdpmi.c
@@ -9,7 +9,7 @@ uint16_t dpmi_alloc(unsigned int par, uint16_t *sel)
 	int tmp;
 	uint16_t seg = __dpmi_allocate_dos_memory(par, &tmp);
 	*sel = tmp;
-	return tmp;
+	return seg;
 }
 
 void dpmi_free(uint16_t sel)

2eb6ea48991f9e5462c0a83b50006e5b58f935a0
John Tsiombikas <nuclear@member.fsf.org>
2022-05-11 04:14:05 +0300

    fix pull.bat

--- pull.bat
+++ pull.bat
@@ -1,22 +1,30 @@
+mkdir src
 rsync -v 192.168.0.4::dosdemo/Makefile Makefile
 rsync -v 192.168.0.4::dosdemo/src/*.c src
 rsync -v 192.168.0.4::dosdemo/src/*.h src
 rsync -v 192.168.0.4::dosdemo/src/*.asm src
+mkdir src\dos
 rsync -v 192.168.0.4::dosdemo/src/dos/*.c src/dos
 rsync -v 192.168.0.4::dosdemo/src/dos/*.h src/dos
 rsync -v 192.168.0.4::dosdemo/src/dos/*.asm src/dos
-rsync -v 192.168.0.4::glutdemo/src/glut/*.c src/glut
-rsync -v 192.168.0.4::glutdemo/src/glut/*.h src/glut
-rsync -v 192.168.0.4::sdldemo/src/sdl/*.c src/sdl
-rsync -v 192.168.0.4::sdldemo/src/sdl/*.h src/sdl
+mkdir src\glut
+rsync -v 192.168.0.4::dosdemo/src/glut/*.c src/glut
+rsync -v 192.168.0.4::dosdemo/src/glut/*.h src/glut
+mkdir src\sdl
+rsync -v 192.168.0.4::dosdemo/src/sdl/*.c src/sdl
+mkdir src\scr
 rsync -v 192.168.0.4::dosdemo/src/scr/*.c src/scr
 rsync -v 192.168.0.4::dosdemo/src/scr/*.h src/scr
+mkdir src\3dgfx
 rsync -v 192.168.0.4::dosdemo/src/3dgfx/*.c src/3dgfx
 rsync -v 192.168.0.4::dosdemo/src/3dgfx/*.h src/3dgfx
 
+mkdir tools
+mkdir tools\csprite
 rsync -v 192.168.0.4::dosdemo/tools/csprite/Makefile tools/csprite/Makefile
 rsync -v 192.168.0.4::dosdemo/tools/csprite/src/*.c tools/csprite/src
 rsync -v 192.168.0.4::dosdemo/tools/csprite/src/*.h tools/csprite/src
 
+mkdir tools\img2bin
 rsync -v 192.168.0.4::dosdemo/tools/img2bin/Makefile tools/img2bin/Makefile
 rsync -v 192.168.0.4::dosdemo/tools/img2bin/*.c tools/img2bin

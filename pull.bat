0d1e0d22e3f1bc97dd6523b78602d886009e2145
John Tsiombikas <nuclear@member.fsf.org>
2022-05-11 04:05:52 +0300

    update pull.bat

--- pull.bat
+++ pull.bat
@@ -5,7 +5,18 @@ rsync -v 192.168.0.4::dosdemo/src/*.asm src
 rsync -v 192.168.0.4::dosdemo/src/dos/*.c src/dos
 rsync -v 192.168.0.4::dosdemo/src/dos/*.h src/dos
 rsync -v 192.168.0.4::dosdemo/src/dos/*.asm src/dos
+rsync -v 192.168.0.4::glutdemo/src/glut/*.c src/glut
+rsync -v 192.168.0.4::glutdemo/src/glut/*.h src/glut
+rsync -v 192.168.0.4::sdldemo/src/sdl/*.c src/sdl
+rsync -v 192.168.0.4::sdldemo/src/sdl/*.h src/sdl
 rsync -v 192.168.0.4::dosdemo/src/scr/*.c src/scr
 rsync -v 192.168.0.4::dosdemo/src/scr/*.h src/scr
 rsync -v 192.168.0.4::dosdemo/src/3dgfx/*.c src/3dgfx
 rsync -v 192.168.0.4::dosdemo/src/3dgfx/*.h src/3dgfx
+
+rsync -v 192.168.0.4::dosdemo/tools/csprite/Makefile tools/csprite/Makefile
+rsync -v 192.168.0.4::dosdemo/tools/csprite/src/*.c tools/csprite/src
+rsync -v 192.168.0.4::dosdemo/tools/csprite/src/*.h tools/csprite/src
+
+rsync -v 192.168.0.4::dosdemo/tools/img2bin/Makefile tools/img2bin/Makefile
+rsync -v 192.168.0.4::dosdemo/tools/img2bin/*.c tools/img2bin

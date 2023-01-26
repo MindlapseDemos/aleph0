ef6fc90042a00d2faa3b749e7c727e695ee37ce9
Michael Kargas <optimus6128@yahoo.gr>
2023-01-26 18:37:17 +0000

    Generate more inbetween palettes for smoother (not jumping) color changing between julia zooming repeat.

--- src/scr/juliatun.c
+++ src/scr/juliatun.c
@@ -9,7 +9,12 @@
 
 #define JULIA_LAYERS 8
 #define JULIA_COLORS 16
-#define JULIA_PAL_COLORS (JULIA_COLORS * JULIA_LAYERS)
+#define JULIA_PAL_TRANSITION_BITS 4
+#define JULIA_PAL_TRANSITIONS (1 << JULIA_PAL_TRANSITION_BITS)
+#define JULIA_PAL_COLORS (JULIA_COLORS * JULIA_LAYERS * JULIA_PAL_TRANSITIONS)
+
+#define JULIA_ANIM_REPEAT_BITS 11
+#define JULIA_ANIM_REPEAT (1 << JULIA_ANIM_REPEAT_BITS)
 
 #define FP_SHR 12
 #define FP_MUL (1 << FP_SHR)
@@ -54,7 +59,7 @@ static struct screen scr = {
 
 static unsigned long startingTime;
 
-static unsigned short *juliaTunelPal;
+static unsigned short *juliaTunnelPal;
 
 static int xp_l[JULIA_LAYERS];
 static int yp_l[JULIA_LAYERS];
@@ -67,7 +72,7 @@ struct screen *juliatunnel_screen(void)
 
 static int init(void)
 {
-	int i,j,k=0;
+	int i,j,k,n=0;
 
 	float sr = 0.0f;
 	float sg = 0.5f;
@@ -77,24 +82,30 @@ static int init(void)
 	float dg = (0.6f - sg) / JULIA_LAYERS;
 	float db = (0.2f - sb) / JULIA_LAYERS;
 
-	juliaTunelPal = (unsigned short*)malloc(sizeof(unsigned short) * JULIA_PAL_COLORS);
+	juliaTunnelPal = (unsigned short*)malloc(sizeof(unsigned short) * JULIA_PAL_COLORS);
 
-	for (j=0; j<JULIA_LAYERS; ++j) {
-		for (i=0; i<JULIA_COLORS; ++i) {
-			int r,g,b;
-			int c = abs(JULIA_COLORS / 2 - 1 - i);
-			if (c >= JULIA_COLORS / 2) c = JULIA_COLORS / 2 - 1;
-			c = JULIA_COLORS / 2 - c - 1;
+	for (k=0; k<JULIA_PAL_TRANSITIONS; ++k) {
+		float d = (float)k / JULIA_PAL_TRANSITIONS;
+		float cr = sr * (1.0f - d) + (sr + dr) * d;
+		float cg = sg * (1.0f - d) + (sg + dg) * d;
+		float cb = sb * (1.0f - d) + (sb + db) * d;
+		for (j=0; j<JULIA_LAYERS; ++j) {
+			for (i=0; i<JULIA_COLORS; ++i) {
+				int r,g,b;
+				int c = abs(JULIA_COLORS / 2 - 1 - i);
+				if (c >= JULIA_COLORS / 2) c = JULIA_COLORS / 2 - 1;
+				c = JULIA_COLORS / 2 - c - 1;
 
-			r = (int)((c << 2) * sr);
-			g = (int)((c << 3) * sg);
-			b = (int)((c << 2) * sb);
+				r = (int)((c << 2) * cr);
+				g = (int)((c << 3) * cg);
+				b = (int)((c << 2) * cb);
 
-			juliaTunelPal[k++] = (r<<11) | (g<<5) | b;
+				juliaTunnelPal[n++] = (r<<11) | (g<<5) | b;
+			}
+			cr += dr;
+			cg += dg;
+			cb += db;
 		}
-		sr += dr;
-		sg += dg;
-		sb += db;
 	}
 
 	return 0;
@@ -163,7 +174,7 @@ static unsigned char renderJuliaPixel(int xk, int yk, int layer_iter)
 	return c + layer_iter * JULIA_COLORS;
 }
 
-static void renderJulia(float scale)
+static void renderJulia(float scale, int palAnimOffset)
 {
 	int x, y;
 
@@ -174,6 +185,8 @@ static void renderJulia(float scale)
 
 	unsigned short *vramUp = (unsigned short*)fb_pixels;
 	unsigned short *vramDown = vramUp + FB_WIDTH * FB_HEIGHT - 1;
+
+	unsigned short *pal = &juliaTunnelPal[JULIA_COLORS * JULIA_LAYERS * palAnimOffset];
 
 	int yk = -di * -screenHeightHalf;
 	int xl = di * -screenWidthHalf;
@@ -186,7 +199,7 @@ static void renderJulia(float scale)
 			const int xki = xk >> DI_BITS;
 			const int yki = yk >> DI_BITS;
 			const unsigned char c = renderJuliaPixel(xki, yki, JULIA_LAYERS);
-			const unsigned short cc = juliaTunelPal[c];
+			const unsigned short cc = pal[c];
 
 			*vramUp++ = cc;
 			*vramDown-- = cc;
@@ -196,27 +209,30 @@ static void renderJulia(float scale)
         yk-=di;
 	}
 }
-//13
+
 static void draw(void)
 {
 	int i;
 
-	//const int t = 2500;
+	//const int t = 3500;
 	//const int layerAdv = 0;
 
 	const int t = time_msec - startingTime;
-	const int layerAdv = t >> 11;
+	const int layerAdv = t >> JULIA_ANIM_REPEAT_BITS;
+	const int palAnimOffset = (t >> (JULIA_ANIM_REPEAT_BITS - JULIA_PAL_TRANSITION_BITS)) & (JULIA_PAL_TRANSITIONS - 1);
 
 	const float tr = 1.0f;
-	const float scale = tr + (float)((t & 2047) / 2047.0f) * tr;
+	const float scale = tr + (float)((t & (JULIA_ANIM_REPEAT-1)) / (float)(JULIA_ANIM_REPEAT-1)) * tr;
 
 	for (i=0; i<JULIA_LAYERS; ++i) {
 		const int ti = (i - layerAdv) << 9;
-		xp_l[i] = (int)(sin((t + ti)/812.0) * FP_MUL) / 3;
-		yp_l[i] = (int)(sin((t + ti)/1482.0) * FP_MUL) / 2;
+		//xp_l[i] = (int)(sin((t + ti)/812.0) * FP_MUL) / 3;
+		//yp_l[i] = (int)(sin((t + ti)/1482.0) * FP_MUL) / 2;
+		xp_l[i] = 1280;
+		yp_l[i] = 1280;
 	}
 
-	renderJulia(scale);
+	renderJulia(scale, palAnimOffset);
 
 	swap_buffers(0);
 }

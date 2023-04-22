59daf4001a562f7f90b224a488961b4b1a4732e4
Michael Kargas <optimus6128@yahoo.gr>
2023-04-22 14:09:01 +0100

    Introduced some more defines to reduced code duplication in fixed vs float tests almost as much as possible

--- src/scr/opt_3d.c
+++ src/scr/opt_3d.c
@@ -12,41 +12,57 @@
 #define VERTICES_DEPTH 32
 #define MAX_VERTEX_ELEMENTS_NUM (VERTICES_WIDTH * VERTICES_HEIGHT * VERTICES_DEPTH)
 
+#define OBJECT_POS_Z 1024
+
+#define AXIS_SHIFT 4
+#define AXIS_HALF_WIDTH ((VERTICES_WIDTH/2)<<AXIS_SHIFT)
+#define AXIS_HALF_HEIGHT ((VERTICES_HEIGHT/2)<<AXIS_SHIFT)
+#define AXIS_HALF_DEPTH ((VERTICES_DEPTH/2)<<AXIS_SHIFT)
+
+
 #define FP_CORE 16
 #define FP_BASE 12
-#define FP_BASE_MUL (1 << FP_BASE)
 #define FP_BASE_TO_CORE (FP_CORE - FP_BASE)
-#define PROJ_SHR 8
+#define PROJ_MUL (1 << 8)
 
-#define FLOAT_TO_FIXED(f,b) ((int)((f) * (1 << b)))
-#define INT_TO_FIXED(i,b) ((i) * (1 << b))
-#define UINT_TO_FIXED(i,b) ((i) << b)
-#define FIXED_TO_INT(x,b) ((x) >> b)
-#define FIXED_TO_FLOAT(x,b) ((float)(x) / (1 << b))
-#define FIXED_MUL(x,y,b) (((x) * (y)) >> b)
-#define FIXED_DIV(x,y,b) (((x) << b) / (y))
-#define FIXED_SQRT(x,b) (isqrt((x) << b))
+#define FLOAT_TO_FIXED(f,b) ((int)((f) * (1 << (b))))
+#define INT_TO_FIXED(i,b) ((i) * (1 << (b)))
+#define UINT_TO_FIXED(i,b) ((i) << (b))
+#define FIXED_TO_INT(x,b) ((x) >> (b))
+#define FIXED_TO_FLOAT(x,b) ((float)(x) / (1 << (b)))
+#define FIXED_MUL(x,y,b) (((x) * (y)) >> (b))
+#define FIXED_DIV(x,y,b) (((x) << (b)) / (y))
+#define FIXED_SQRT(x,b) (isqrt((x) << (b)))
 
 #define DEG_TO_RAD_256(x) (((2 * M_PI) * (x)) / 256)
 #define RAD_TO_DEG_256(x) ((256 * (x)) / (2 * M_PI))
 
 
-//#define FIXED_TEST
-//#define DO_ALL_TOGETHER
-#define AXES_TEST
+#define FIXED_TEST
+#define DO_ALL_TOGETHER
+//#define AXES_TEST
 
-#define OBJECT_POS_Z 1024
-
+#ifdef FIXED_TEST
+	#define VECTYPE_AFTER_MUL_ADDS(x,b)		FIXED_TO_INT(x,b)
+	#define VECTYPE_ROUND(x)				(x)
+	#define FLOAT_TO_VECTYPE(x,b)			FLOAT_TO_FIXED(x,b)
+#else
+	#define VECTYPE_AFTER_MUL_ADDS(x,b)		(x)
+	#define VECTYPE_ROUND(x)				(cround64(x))
+	#define FLOAT_TO_VECTYPE(x,b)			(x)
+#endif
 
 typedef float real;
 
+#ifdef FIXED_TEST
+	typedef int vecType;
+#else
+	typedef real vecType;
+#endif
+
 typedef struct OptVertex
 {
-	#ifdef FIXED_TEST
-		int x,y,z;
-	#else
-		real x, y, z;
-	#endif
+	vecType x,y,z;
 }OptVertex;
 
 #define SET_OPT_VERTEX(xp,yp,zp,v) v->x = (xp); v->y = (yp); v->z = (zp);
@@ -57,22 +73,22 @@ static OptVertex *screenVertices;
 static OptVertex *axisVerticesX, *axisVerticesY, *axisVerticesZ;
 
 
-#ifdef FIXED_TEST
-	static void createRotationMatrixValues(int rotX, int rotY, int rotZ, int *mat)
-	{
-		int *rotVecs = (int*)mat;
+static void createRotationMatrixValues(vecType rotX, vecType rotY, vecType rotZ, vecType *mat)
+{
+	vecType *rotVecs = (vecType*)mat;
 
-		real aX = (real)(DEG_TO_RAD_256((real)rotX / 64.0f));
-		real aY = (real)(DEG_TO_RAD_256((real)rotY / 64.0f));
-		real aZ = (real)(DEG_TO_RAD_256((real)rotZ / 64.0f));
+	const real aX = (real)(DEG_TO_RAD_256((real)rotX / 64.0f));
+	const real aY = (real)(DEG_TO_RAD_256((real)rotY / 64.0f));
+	const real aZ = (real)(DEG_TO_RAD_256((real)rotZ / 64.0f));
 
-		const int cosxr = (int)(cos(aX) * FP_BASE_MUL);
-		const int cosyr = (int)(cos(aY) * FP_BASE_MUL);
-		const int coszr = (int)(cos(aZ) * FP_BASE_MUL);
-		const int sinxr = (int)(sin(aX) * FP_BASE_MUL);
-		const int sinyr = (int)(sin(aY) * FP_BASE_MUL);
-		const int sinzr = (int)(sin(aZ) * FP_BASE_MUL);
+	const vecType cosxr = (vecType)(FLOAT_TO_VECTYPE(cos(aX), FP_BASE));
+	const vecType cosyr = (vecType)(FLOAT_TO_VECTYPE(cos(aY), FP_BASE));
+	const vecType coszr = (vecType)(FLOAT_TO_VECTYPE(cos(aZ), FP_BASE));
+	const vecType sinxr = (vecType)(FLOAT_TO_VECTYPE(sin(aX), FP_BASE));
+	const vecType sinyr = (vecType)(FLOAT_TO_VECTYPE(sin(aY), FP_BASE));
+	const vecType sinzr = (vecType)(FLOAT_TO_VECTYPE(sin(aZ), FP_BASE));
 
+	#ifdef FIXED_TEST
 		*rotVecs++ = (FIXED_MUL(cosyr, coszr, FP_BASE)) << FP_BASE_TO_CORE;
 		*rotVecs++ = (FIXED_MUL(FIXED_MUL(sinxr, sinyr, FP_BASE), coszr, FP_BASE) - FIXED_MUL(cosxr, sinzr, FP_BASE)) << FP_BASE_TO_CORE;
 		*rotVecs++ = (FIXED_MUL(FIXED_MUL(cosxr, sinyr, FP_BASE), coszr, FP_BASE) + FIXED_MUL(sinxr, sinzr, FP_BASE)) << FP_BASE_TO_CORE;
@@ -82,19 +98,7 @@ static OptVertex *axisVerticesX, *axisVerticesY, *axisVerticesZ;
 		*rotVecs++ = (-sinyr) << FP_BASE_TO_CORE;
 		*rotVecs++ = (FIXED_MUL(sinxr, cosyr, FP_BASE)) << FP_BASE_TO_CORE;
 		*rotVecs = (FIXED_MUL(cosxr, cosyr, FP_BASE)) << FP_BASE_TO_CORE;
-	}
-#else
-	static void createRotationMatrixValues(real rotX, real rotY, real rotZ, real *mat)
-	{
-		real *rotVecs = (real*)mat;
-
-		const real cosxr = (real)cos(rotX);
-		const real cosyr = (real)cos(rotY);
-		const real coszr = (real)cos(rotZ);
-		const real sinxr = (real)sin(rotX);
-		const real sinyr = (real)sin(rotY);
-		const real sinzr = (real)sin(rotZ);
-
+	#else
 		*rotVecs++ = cosyr * coszr;
 		*rotVecs++ = sinxr * sinyr * coszr - cosxr * sinzr;
 		*rotVecs++ = cosxr * sinyr * coszr + sinxr * sinzr;
@@ -105,94 +109,48 @@ static OptVertex *axisVerticesX, *axisVerticesY, *axisVerticesZ;
 		*rotVecs++ = -sinyr;
 		*rotVecs++ = sinxr * cosyr;
 		*rotVecs = cosxr * cosyr;
-	}
-#endif
-
-#ifdef FIXED_TEST
-	static void MulManyVec3Mat33(OptVertex *dst, OptVertex *src, int *m, int count)
-	{
-		int i;
-		for (i = 0; i < count; ++i)
-		{
-			const int x = src->x;
-			const int y = src->y;
-			const int z = src->z;
-
-			dst->x = FIXED_TO_INT(x * m[0] + y * m[3] + z * m[6], FP_CORE);
-			dst->y = FIXED_TO_INT(x * m[1] + y * m[4] + z * m[7], FP_CORE);
-			dst->z = FIXED_TO_INT(x * m[2] + y * m[5] + z * m[8], FP_CORE);
-
-			++src;
-			++dst;
-		}
-	}
-#else
-	static void MulManyVec3Mat33(OptVertex *dst, OptVertex *src, real *m, int count)
-	{
-		int i;
-		for (i = 0; i < count; ++i)
-		{
-			const real x = src->x;
-			const real y = src->y;
-			const real z = src->z;
-
-			dst->x = x * m[0] + y * m[3] + z * m[6];
-			dst->y = x * m[1] + y * m[4] + z * m[7];
-			dst->z = x * m[2] + y * m[5] + z * m[8];
-
-			++src;
-			++dst;
-		}
-	}
-#endif
-
-#ifdef FIXED_TEST
-static void translateAndProjectVertices(int count)
-{
-	int i;
-	const int offsetX = FB_WIDTH >> 1;
-	const int offsetY = FB_HEIGHT >> 1;
-
-	for (i=0; i<count; i++) {
-		const int vz = screenVertices[i].z + OBJECT_POS_Z;
-		screenVertices[i].z = vz;
-		if (vz > 0) {
-			screenVertices[i].x = offsetX + (screenVertices[i].x << PROJ_SHR) / vz;
-			screenVertices[i].y = offsetY + (screenVertices[i].y << PROJ_SHR) / vz;
-		}
-	}
-}
-#else
-static void translateAndProjectVertices(int count)
-{
-	int i;
-	const real offsetX = (real)(FB_WIDTH >> 1);
-	const real offsetY = (real)(FB_HEIGHT >> 1);
-
-	for (i=0; i<count; i++) {
-		const real vz = screenVertices[i].z + OBJECT_POS_Z;
-		screenVertices[i].z = vz;
-		if (vz > 0) {
-			screenVertices[i].x = offsetX + (screenVertices[i].x * (1 << PROJ_SHR)) / vz;
-			screenVertices[i].y = offsetY + (screenVertices[i].y * (1 << PROJ_SHR)) / vz;
-		}
-	}
-}
-#endif
-
-static void rotateVertices(int ticks, int count)
-{
-	const int t = ticks >> 1;
-
-	#ifdef FIXED_TEST
-		static int rotMat[9];
-
-		createRotationMatrixValues(t, 2*t, 3*t, rotMat);
-	#else
-		static real rotMat[9];
-
-		createRotationMatrixValues((real)DEG_TO_RAD_256(t) / 64.0f, (real)DEG_TO_RAD_256(2*t) / 64.0f, (real)DEG_TO_RAD_256(3*t) / 64.0f, rotMat);
 	#endif
+}
+
+static void MulManyVec3Mat33(OptVertex *dst, OptVertex *src, vecType *m, int count)
+{
+	int i;
+	for (i = 0; i < count; ++i)
+	{
+		const vecType x = src->x;
+		const vecType y = src->y;
+		const vecType z = src->z;
+
+		dst->x = VECTYPE_AFTER_MUL_ADDS(x * m[0] + y * m[3] + z * m[6], FP_CORE);
+		dst->y = VECTYPE_AFTER_MUL_ADDS(x * m[1] + y * m[4] + z * m[7], FP_CORE);
+		dst->z = VECTYPE_AFTER_MUL_ADDS(x * m[2] + y * m[5] + z * m[8], FP_CORE);
+
+		++src;
+		++dst;
+	}
+}
+
+static void translateAndProjectVertices(int count)
+{
+	int i;
+	const vecType offsetX = (vecType)(FB_WIDTH >> 1);
+	const vecType offsetY = (vecType)(FB_HEIGHT >> 1);
+
+	for (i=0; i<count; i++) {
+		const vecType vz = screenVertices[i].z + OBJECT_POS_Z;
+		screenVertices[i].z = vz;
+		if (vz > 0) {
+			screenVertices[i].x = offsetX + (screenVertices[i].x * PROJ_MUL) / vz;
+			screenVertices[i].y = offsetY + (screenVertices[i].y * PROJ_MUL) / vz;
+		}
+	}
+}
+
+static void rotateVertices(int count, int t)
+{
+	static vecType rotMat[9];
+
+	createRotationMatrixValues(t, 2*t, 3*t, rotMat);
 
 	MulManyVec3Mat33(screenVertices, objectVertices, rotMat, count);
 }
@@ -204,15 +162,10 @@ static void renderVertices(int count)
 
 	OptVertex* v = screenVertices;
 	for (i = 0; i < count; i++) {
-		#ifdef FIXED_TEST
-			const int x = v->x;
-			const int y = v->y;
-			const int z = v->z;
-		#else
-			const int x = cround64(v->x);
-			const int y = cround64(v->y);
-			const int z = cround64(v->z);
-		#endif
+		const int x = VECTYPE_ROUND(v->x);
+		const int y = VECTYPE_ROUND(v->y);
+		const int z = VECTYPE_ROUND(v->z);
+
 		if (z > 0 && x >= 0 && x < FB_WIDTH && y >= 0 && y < FB_HEIGHT) {
 			*(dst + y * FB_WIDTH + x) = 0xFFFF;
 		}
@@ -220,61 +173,34 @@ static void renderVertices(int count)
 	}
 }
 
-static void doAllTogether(OptVertex *v, int count, int ticks)
+static void doAllTogether(OptVertex *v, int count, int t)
 {
+	static vecType m[9];
+
 	unsigned short* dst = (unsigned short*)fb_pixels;
-	const int t = ticks >> 1;
 	int i;
 
-	#ifdef FIXED_TEST
-		static int m[9];
-		const int offsetX = FB_WIDTH >> 1;
-		const int offsetY = FB_HEIGHT >> 1;
+	const vecType offsetX = (vecType)(FB_WIDTH >> 1);
+	const vecType offsetY = (vecType)(FB_HEIGHT >> 1);
 
-		createRotationMatrixValues(t, 2*t, 3*t, m);
-	#else
-		static real m[9];
-		const real offsetX = (real)(FB_WIDTH >> 1);
-		const real offsetY = (real)(FB_HEIGHT >> 1);
+	createRotationMatrixValues(t, 2*t, 3*t, m);
 
-		createRotationMatrixValues((real)DEG_TO_RAD_256(t) / 64.0f, (real)DEG_TO_RAD_256(2*t) / 64.0f, (real)DEG_TO_RAD_256(3*t) / 64.0f, m);
-	#endif
+	for (i = 0; i < count; ++i) {
+		const vecType x = v->x;
+		const vecType y = v->y;
+		const vecType z = v->z;
 
-	#ifdef FIXED_TEST
-		for (i = 0; i < count; ++i) {
-			const int x = v->x;
-			const int y = v->y;
-			const int z = v->z;
+		const vecType sz = VECTYPE_AFTER_MUL_ADDS(x * m[2] + y * m[5] + z * m[8], FP_CORE) + OBJECT_POS_Z;
+		if (sz > 0) {
+			const int sx = offsetX + ((VECTYPE_AFTER_MUL_ADDS(x * m[0] + y * m[3] + z * m[6], FP_CORE)) * PROJ_MUL) / sz;
+			const int sy = offsetY + ((VECTYPE_AFTER_MUL_ADDS(x * m[1] + y * m[4] + z * m[7], FP_CORE)) * PROJ_MUL) / sz;
 
-			const int sz = FIXED_TO_INT(x * m[2] + y * m[5] + z * m[8], FP_CORE) + OBJECT_POS_Z;
-			if (sz > 0) {
-				const int sx = offsetX + ((FIXED_TO_INT(x * m[0] + y * m[3] + z * m[6], FP_CORE)) << PROJ_SHR) / sz;
-				const int sy = offsetY + ((FIXED_TO_INT(x * m[1] + y * m[4] + z * m[7], FP_CORE)) << PROJ_SHR) / sz;
-
-				if (sx >= 0 && sx < FB_WIDTH && sy >= 0 && sy < FB_HEIGHT) {
-					*(dst + sy * FB_WIDTH + sx) = 0xFFFF;
-				}
+			if (sx >= 0 && sx < FB_WIDTH && sy >= 0 && sy < FB_HEIGHT) {
+				*(dst + sy * FB_WIDTH + sx) = 0xFFFF;
 			}
-			++v;
 		}
-	#else
-		for (i = 0; i < count; ++i) {
-			const real x = v->x;
-			const real y = v->y;
-			const real z = v->z;
-
-			const real sz = x * m[2] + y * m[5] + z * m[8] + OBJECT_POS_Z;
-			if (sz > 0) {
-				const int sx = cround64(offsetX + ((x * m[0] + y * m[3] + z * m[6]) * (1 << PROJ_SHR)) / sz);
-				const int sy = cround64(offsetY + ((x * m[1] + y * m[4] + z * m[7]) * (1 << PROJ_SHR)) / sz);
-
-				if (sx >= 0 && sx < FB_WIDTH && sy >= 0 && sy < FB_HEIGHT) {
-					*(dst + sy * FB_WIDTH + sx) = 0xFFFF;
-				}
-			}
-			++v;
-		}
-	#endif
+		++v;
+	}
 }
 
 static void initObject3D()
@@ -289,11 +215,26 @@ static void initObject3D()
 	for (z = -VERTICES_DEPTH / 2; z < VERTICES_DEPTH / 2; ++z) {
 		for (y = -VERTICES_HEIGHT / 2; y < VERTICES_HEIGHT / 2; ++y) {
 			for (x = -VERTICES_WIDTH / 2; x < VERTICES_WIDTH / 2; ++x) {
-				SET_OPT_VERTEX(x<<4,y<<4,z<<4,v)
+				SET_OPT_VERTEX(x<<AXIS_SHIFT,y<<AXIS_SHIFT,z<<AXIS_SHIFT,v)
 				++v;
 			}
 		}
 	}
+}
+
+static void generateAxisVertices(OptVertex *axisStart, OptVertex *axisStep, OptVertex *axisDst, int count)
+{
+/*	int x = axisStart->x;
+	int y = axisStart->y;
+	int z = axisStart->z;
+	const int 
+	do {
+	} while(--count != 0);*/
+}
+
+static void generateAxesVertices(OptVertex *axesEnds)
+{
+	int i;
 }
 
 static void initAxes3D()
@@ -308,9 +249,9 @@ static void initAxes3D()
 	axisVerticesZ = (OptVertex*)malloc(VERTICES_DEPTH * sizeof(OptVertex));
 
 	v = objectVertices;
-	SET_OPT_VERTEX((VERTICES_WIDTH/2)<<4,0,0,v)	++v;
-	SET_OPT_VERTEX(0,(VERTICES_HEIGHT/2)<<4,0,v)	++v;
-	SET_OPT_VERTEX(0,0,(VERTICES_DEPTH/2)<<4,v)
+	SET_OPT_VERTEX(AXIS_HALF_WIDTH,0,0,v)	++v;
+	SET_OPT_VERTEX(0,AXIS_HALF_HEIGHT,0,v)	++v;
+	SET_OPT_VERTEX(0,0,AXIS_HALF_DEPTH,v)
 }
 
 void Opt3DinitPerfTest()
@@ -337,12 +278,13 @@ void Opt3DrunPerfTest(int ticks)
 	memset(fb_pixels, 0, FB_WIDTH * FB_HEIGHT * 2);
 
 	#ifdef AXES_TEST
-		doAllTogether(objectVertices, 3, ticks);
+		rotateVertices(3, ticks);
+		generateAxesVertices(screenVertices);
 	#else
 		#ifdef DO_ALL_TOGETHER
 			doAllTogether(objectVertices, MAX_VERTEX_ELEMENTS_NUM, ticks);
 		#else
-			rotateVertices(ticks, MAX_VERTEX_ELEMENTS_NUM);
+			rotateVertices(MAX_VERTEX_ELEMENTS_NUM, ticks);
 			translateAndProjectVertices(MAX_VERTEX_ELEMENTS_NUM);
 			renderVertices(MAX_VERTEX_ELEMENTS_NUM);
 		#endif

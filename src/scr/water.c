4d7623617387113b58dba248297e13c40496cd75
BugoTheCat <optimus6128@yahoo.gr>
2024-05-08 13:57:28 +0100

    Pseudo reflect on water test.

--- src/scr/water.c
+++ src/scr/water.c
@@ -26,8 +26,8 @@ static struct screen scr = {
 	draw
 };
 
-#define WATER_WIDTH 256
-#define WATER_HEIGHT 256
+#define WATER_TEX_WIDTH 256
+#define WATER_TEX_HEIGHT 256
 
 #define CLOUD_TEX_WIDTH 256
 #define CLOUD_TEX_HEIGHT 256
@@ -35,7 +35,7 @@ static struct screen scr = {
 #define CLOUD_SHADES 32
 
 #define SKY_HEIGHT 8192
-#define WATER_FLOOR 4096
+#define WATER_FLOOR 2048
 #define WATER_SHADES 16
 #define SKY_PROJ 256
 
@@ -124,7 +124,7 @@ static void initCloudTex()
 static int init(void)
 {
 	int i;
-	const int size = WATER_WIDTH * WATER_HEIGHT;
+	const int size = WATER_TEX_WIDTH * WATER_TEX_HEIGHT;
 
 	waterBuffer1 = (unsigned char*)malloc(size);
 	waterBuffer2 = (unsigned char*)malloc(size);
@@ -135,10 +135,10 @@ static int init(void)
 	wb2 = waterBuffer2;
 
 	for (i = 0; i < WATER_SHADES; ++i) {
-		float s = 1.0f - (float)(WATER_SHADES - i - 1) / (WATER_SHADES * 2.0);
+		float s = 1.0f - (float)(WATER_SHADES - i - 1) / (WATER_SHADES * 1.125);
 		CLAMP01(s)
 		waterPal[i] = (unsigned short*)malloc(sizeof(unsigned short) * 256);
-		setPalGradient(0, 127, 3*s, 3*s, 9*s, 31 * s, 63 * s, 31 * s, waterPal[i]);
+		setPalGradient(0, 127, 0, 0, 7*s, 31 * s, 63 * s, 31 * s, waterPal[i]);
 		setPalGradient(128, 255, 0, 0, 0, 0, 0, 0, waterPal[i]);
 	}
 
@@ -178,17 +178,17 @@ void updateWaterAsm5(void *buffer1, void *buffer2, void *vramStart);
 #else
 static void updateWater32(unsigned char *buffer1, unsigned char *buffer2)
 {
-	int count = (WATER_WIDTH / 4) * (WATER_HEIGHT - 2) - 2;
+	int count = (WATER_TEX_WIDTH / 4) * (WATER_TEX_HEIGHT - 2) - 2;
 
 	unsigned int *src1 = (unsigned int*)buffer1;
 	unsigned int *src2 = (unsigned int*)buffer2;
-	unsigned int *vram = (unsigned int*)((unsigned char*)wb1 + WATER_WIDTH + 4);
+	unsigned int *vram = (unsigned int*)((unsigned char*)wb1 + WATER_TEX_WIDTH + 4);
 
 	do {
 		const unsigned int c0 = *(unsigned int*)((unsigned char*)src1-1);
 		const unsigned int c1 = *(unsigned int*)((unsigned char*)src1+1);
-		const unsigned int c2 = *(src1-(WATER_WIDTH / 4));
-		const unsigned int c3 = *(src1+(WATER_WIDTH / 4));
+		const unsigned int c2 = *(src1-(WATER_TEX_WIDTH / 4));
+		const unsigned int c3 = *(src1+(WATER_TEX_WIDTH / 4));
 
 		// Subtract and then absolute value of 4 bytes packed in 8bits (From Hacker's Delight)
 		const unsigned int c = (((c0 + c1 + c2 + c3) >> 1) & 0x7f7f7f7f) - *src2;
@@ -205,13 +205,13 @@ static void updateWater32(unsigned char *buffer1, unsigned char *buffer2)
 static void renderBlob(int xp, int yp, unsigned char *buffer)
 {
 	int i,j;
-	unsigned char *dst = buffer + yp * WATER_WIDTH + xp;
+	unsigned char *dst = buffer + yp * WATER_TEX_WIDTH + xp;
 
 	for (j=0; j<3; ++j) {
 		for (i=0; i<3; ++i) {
 			*(dst + i) = 0x3f;
 		}
-		dst += WATER_WIDTH;
+		dst += WATER_TEX_WIDTH;
 	}
 }
 
@@ -222,8 +222,8 @@ static void makeRipples(unsigned char *buff, int t)
 	renderBlob(24 + (rand() % 224), 24 + (rand() % 224), buff);
 
 	/*for (i = 0; i<3; ++i) {
-		const int xp = WATER_WIDTH / 2 + (int)(sin((t / 32 + 64*i)/24.0) * 96);
-		const int yp = WATER_HEIGHT / 2 + (int)(sin((t / 32 + 64*i)/16.0) * 64);
+		const int xp = WATER_TEX_WIDTH / 2 + (int)(sin((t / 32 + 64*i)/24.0) * 96);
+		const int yp = WATER_TEX_HEIGHT / 2 + (int)(sin((t / 32 + 64*i)/16.0) * 64);
 
 		renderBlob(xp,yp,buff);
 	}*/
@@ -234,9 +234,9 @@ static void runWaterEffect(int t)
 	static int prevT = 0;
 	const int dt = t - prevT;
 	if (dt < 0 || dt > 20) {
-		unsigned char* buff1 = wb1 + WATER_WIDTH + 4;
-		unsigned char* buff2 = wb2 + WATER_WIDTH + 4;
-		unsigned char* vramOffset = (unsigned char*)buff1 + WATER_WIDTH + 4;
+		unsigned char* buff1 = wb1 + WATER_TEX_WIDTH + 4;
+		unsigned char* buff2 = wb2 + WATER_TEX_WIDTH + 4;
+		unsigned char* vramOffset = (unsigned char*)buff1 + WATER_TEX_WIDTH + 4;
 
 		makeRipples(buff1, t);
 
@@ -257,6 +257,17 @@ static void renderBitmapLineX(int u, int du, int texWidth, int length, unsigned 
 	while (length-- > 0) {
 		int tu = (u >> FP_SCALE) & (texWidth - 1);
 		*dst++ = pal[src[tu]];
+		u += du;
+	};
+}
+
+static void renderBitmapLineX2(int u, int du, int texWidth1, int texWidth2, int length, unsigned char* src1, unsigned char* src2, unsigned short* pal, unsigned short* dst)
+{
+	while (length-- > 0) {
+		int tu = u >> FP_SCALE;
+		int tu1 = tu & (texWidth1 - 1);
+		int tu2 = tu & (texWidth2 - 1);
+		*dst++ = pal[src1[tu1]] + pal[src2[tu2]>>2];
 		u += du;
 	};
 }
@@ -313,21 +324,23 @@ static void testBlitCloudWater()
 		unsigned char* src;
 		unsigned short* dst = (unsigned short*)fb_pixels + y * FB_WIDTH;
 
-		int z, u, v, du;
+		int z, u, v, v1, v2, du;
 		int palNum = (WATER_SHADES * (y - FB_HEIGHT / 2)) / (FB_HEIGHT / 2);
 
 		int yp = FB_HEIGHT / 2 - y;
 		if (yp == 0) yp = 1;
 		z = (WATER_FLOOR * SKY_PROJ) / yp;
 
-		du = z * 2;
+		du = z * 8;
 		u = (-FB_WIDTH / 2) * du;
 
-		v = (z >> 7) & (WATER_HEIGHT - 1);
-		src = &waterTex[v * WATER_WIDTH];
+		v = z >> 6;
+		v1 = v & (WATER_TEX_HEIGHT - 1);
+		v2 = (CLOUD_TEX_HEIGHT - 1) - v & (CLOUD_TEX_HEIGHT - 1);
+		src = &waterTex[v1 * WATER_TEX_WIDTH];
 
 		CLAMP(palNum, 0, WATER_SHADES - 1)
-		renderBitmapLineX(u, du, WATER_WIDTH, FB_WIDTH, src, waterPal[palNum], dst);
+		renderBitmapLineX2(u, du, WATER_TEX_WIDTH, CLOUD_TEX_WIDTH, FB_WIDTH, src, &skyTex[v2 * CLOUD_TEX_WIDTH], waterPal[palNum], dst);
 	}
 }
 

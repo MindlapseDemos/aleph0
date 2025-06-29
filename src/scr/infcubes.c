65e39d8288e65f2d3890b9ed3d1e72dcca9dc4dd
John Tsiombikas <nuclear@member.fsf.org>
2025-06-29 06:27:24 +0300

    infcubes motion

--- src/scr/infcubes.c
+++ src/scr/infcubes.c
@@ -10,6 +10,7 @@
 #include "imago2.h"
 #include "gfxutil.h"
 #include "mesh.h"
+#include "noise.h"
 
 static int init(void);
 static void destroy(void);
@@ -26,10 +27,12 @@ static struct screen scr = {
 	draw
 };
 
-static float cam_theta = -29, cam_phi = 35;
-static float cam_dist = 6;
+/*static float cam_theta = -29, cam_phi = 35;
+static float cam_dist = 6;*/
 static struct pimage tex_inner, tex_outer;
 static struct g3d_mesh mesh_cube, mesh_cube2;
+
+static dseq_event *ev_zoom;
 
 struct screen *infcubes_screen(void)
 {
@@ -72,6 +75,8 @@ static int init(void)
 	}
 	apply_mesh_xform(&mesh_cube2, scalemat);
 	normalize_mesh_normals(&mesh_cube2);
+
+	ev_zoom = dseq_lookup("infcubes.zoom");
 	return 0;
 }
 
@@ -82,36 +87,27 @@ static void destroy(void)
 
 static void start(long trans_time)
 {
-	g3d_matrix_mode(G3D_PROJECTION);
-	g3d_load_identity();
-	g3d_perspective(70.0, 1.3333333, 0.5, 100.0);
-
 	g3d_enable(G3D_CULL_FACE);
 	g3d_disable(G3D_LIGHTING);
 	g3d_enable(G3D_LIGHT0);
 	g3d_disable(G3D_DEPTH_TEST);
 }
 
-static void update(void)
-{
-	mouse_orbit_update(&cam_theta, &cam_phi, &cam_dist);
-}
-
 static void draw(void)
 {
 	float t = (float)time_msec / 16.0f;
-	update();
+	float zoom;
+
+	zoom = dseq_value(ev_zoom);
+
+	g3d_matrix_mode(G3D_PROJECTION);
+	g3d_load_identity();
+	g3d_perspective(70.0 + zoom * 70.0f, 1.3333333, 0.5, 100.0);
 
 	g3d_matrix_mode(G3D_MODELVIEW);
 	g3d_load_identity();
-	g3d_translate(0, 0, -cam_dist);
-	g3d_rotate(cam_phi, 1, 0, 0);
-	g3d_rotate(cam_theta, 0, 1, 0);
-	if(opt.sball) {
-		g3d_mult_matrix(sball_matrix);
-	}
 
-	/*memset(fb_pixels, 0, FB_WIDTH * FB_HEIGHT * 2);*/
+	g3d_translate(0, 0, -6);
 
 	g3d_polygon_mode(G3D_FLAT);
 	g3d_enable(G3D_TEXTURE_2D);
@@ -124,6 +120,9 @@ static void draw(void)
 	draw_mesh(&mesh_cube2);
 	g3d_pop_matrix();
 
+	g3d_translate(noise1(t * 0.05) * 2, noise1(t * 0.05 + 5.0f) * 2, 0);
+	g3d_rotate(t * 1.5f, 1, 0, 0);
+	g3d_rotate(t * 1.5f, 0, 1, 0);
 	g3d_set_texture(tex_inner.width, tex_inner.height, tex_inner.pixels);
 	draw_mesh(&mesh_cube);
 	g3d_disable(G3D_TEXTURE_GEN);

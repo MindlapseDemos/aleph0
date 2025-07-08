b7cd94591e8222bf5f70652bf74e9cd828395250
BugoTheCat <optimus6128@yahoo.gr>
2025-07-08 18:54:33 +0100

    Made the bump scroll faster (and also going reversed direction which I think I prefer) Also forgot to mention, in previous commit I also trimmed the water part a bit and clouds/rain/object start almost immediatelly, no wait.

--- src/scr/bump.c
+++ src/scr/bump.c
@@ -858,7 +858,7 @@ static void draw(void)
 
 		fadeToBlack16bpp(ft, lightmap + LMAP_OFFSET_Y * LMAP_WIDTH + LMAP_OFFSET_X, FB_WIDTH, FB_HEIGHT, LMAP_WIDTH);
 
-		blitBumpTextureScript(t);
+		blitBumpTextureScript(-2*t);
 
 		renderBump((unsigned short*)fb_pixels);
 	} else {

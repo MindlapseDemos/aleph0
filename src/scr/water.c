98f2ff6b616258cac543a6380f4120b47c0a0823
BugoTheCat <optimus6128@yahoo.gr>
2025-07-10 18:16:53 +0100

    I broke things, let's revert

--- src/scr/water.c
+++ src/scr/water.c
@@ -271,7 +271,7 @@ static void start(long trans_time)
 	startingTime = time_msec;
 }
 
-#ifndef _MSC_VER
+#ifdef __WATCOMC__
 void updateWaterAsm5(void* buffer1, void* buffer2, void* vramStart);
 #else
 static void updateWater32(unsigned char* buffer1, unsigned char* buffer2)
@@ -369,7 +369,7 @@ static void runWaterEffect(int t)
 
 		makeRipples(buff1, t);
 
-#ifndef _MSC_VER
+#ifdef __WATCOMC__
 		updateWaterAsm5(buff1, buff2, vramOffset);
 #else
 		updateWater32(buff1, buff2);
